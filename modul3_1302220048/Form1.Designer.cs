72fb58442e14d387165d5f92b2847e602f858ffe
unknown <ramdhanaa44@gmail.com>
2024-03-07 10:42:43 +0700

    menambahkan layout GUI kalkultor

---
+++ modul3_1302220048/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace modul3_1302220048
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button12 = new Button();
+            button11 = new Button();
+            button10 = new Button();
+            button9 = new Button();
+            button8 = new Button();
+            button7 = new Button();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button12);
+            panel1.Controls.Add(button11);
+            panel1.Controls.Add(button10);
+            panel1.Controls.Add(button9);
+            panel1.Controls.Add(button8);
+            panel1.Controls.Add(button7);
+            panel1.Controls.Add(button6);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(textBox1);
+            panel1.Location = new Point(212, 187);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(261, 160);
+            panel1.TabIndex = 10;
+            panel1.Paint += panel1_Paint;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(147, 111);
+            button12.Name = "button12";
+            button12.Size = new Size(46, 23);
+            button12.TabIndex = 22;
+            button12.Text = "9";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(102, 111);
+            button11.Name = "button11";
+            button11.Size = new Size(46, 23);
+            button11.TabIndex = 21;
+            button11.Text = "8";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(59, 111);
+            button10.Name = "button10";
+            button10.Size = new Size(46, 23);
+            button10.TabIndex = 20;
+            button10.Text = "7";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(147, 91);
+            button9.Name = "button9";
+            button9.Size = new Size(46, 23);
+            button9.TabIndex = 19;
+            button9.Text = "6";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(102, 91);
+            button8.Name = "button8";
+            button8.Size = new Size(46, 23);
+            button8.TabIndex = 18;
+            button8.Text = "5";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(59, 91);
+            button7.Name = "button7";
+            button7.Size = new Size(46, 23);
+            button7.TabIndex = 17;
+            button7.Text = "4";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(147, 72);
+            button6.Name = "button6";
+            button6.Size = new Size(46, 23);
+            button6.TabIndex = 16;
+            button6.Text = "3";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(102, 72);
+            button5.Name = "button5";
+            button5.Size = new Size(46, 23);
+            button5.TabIndex = 15;
+            button5.Text = "2";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(59, 72);
+            button4.Name = "button4";
+            button4.Size = new Size(46, 23);
+            button4.TabIndex = 14;
+            button4.Text = "1";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(147, 52);
+            button3.Name = "button3";
+            button3.Size = new Size(46, 23);
+            button3.TabIndex = 13;
+            button3.Text = "0";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(102, 52);
+            button2.Name = "button2";
+            button2.Size = new Size(46, 23);
+            button2.TabIndex = 12;
+            button2.Text = "=";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(59, 52);
+            button1.Name = "button1";
+            button1.Size = new Size(46, 23);
+            button1.TabIndex = 11;
+            button1.Text = "+";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(75, 23);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 11;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Panel panel1;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button4;
+        private Button button3;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Button button12;
+        private Button button11;
+        private Button button10;
+        private Button button9;
+        private Button button8;
+    }
+}
---
+++ modul3_1302220048/Form1.cs
@@ -0,0 +1,90 @@
+namespace modul3_1302220048
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
